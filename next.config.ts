8437dfd31ca5f0c1b8ee5bcdd1016cc9eafa1c0b
Mike Greenfield <mwg1378@gmail.com>
2026-05-01 15:07:33 -0700

    Include Prisma engine in Vercel function bundle

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  outputFileTracingIncludes: {
+    '/**/*': ['./lib/generated/prisma/**/*'],
+  },
+  serverExternalPackages: ['@prisma/client'],
+}
 
-export default nextConfig;
+export default nextConfig
